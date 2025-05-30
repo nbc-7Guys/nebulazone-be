aff78b605958e3fb22531032fc97b67f17bd7ce7
SeungHyunLee054 <yellow13lsh@naver.com>
2025-05-30 16:25:36 +0900

    Refactor: builder 수정
    클래스로 이동

--- src/main/java/nbc/chillguys/nebulazone/domain/user/entity/Address.java
+++ src/main/java/nbc/chillguys/nebulazone/domain/user/entity/Address.java
@@ -3,12 +3,15 @@ package nbc.chillguys.nebulazone.domain.user.entity;
 import java.util.Objects;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
+@Builder
+@AllArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class Address {
 	private String roadAddress;
@@ -16,13 +19,6 @@ public class Address {
 	private String detailAddress;
 
 	private String addressNickname;
-
-	@Builder
-	public Address(String roadAddress, String detailAddress, String addressNickname) {
-		this.roadAddress = roadAddress;
-		this.detailAddress = detailAddress;
-		this.addressNickname = addressNickname;
-	}
 
 	@Override
 	public boolean equals(Object object) {
