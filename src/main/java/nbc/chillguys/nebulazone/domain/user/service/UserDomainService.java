d0209ba6f3568674f51eb31e210f29e13442cac4
SeungHyunLee054 <yellow13lsh@naver.com>
2025-05-30 14:16:57 +0900

    Comment: javadoc 추가
    javadoc 추가

--- src/main/java/nbc/chillguys/nebulazone/domain/user/service/UserDomainService.java
+++ src/main/java/nbc/chillguys/nebulazone/domain/user/service/UserDomainService.java
@@ -22,6 +22,14 @@ public class UserDomainService {
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
 
+	/**
+	 * 탈퇴되지 않은 유저를 email로 조회<br>
+	 * 조회 시 roles도 함께 fetch
+	 * @param email 이메일
+	 * @throws UserException 없을 시 예외 발생
+	 * @return id, email, password, roles
+	 * @author 이승현
+	 */
 	public UserSignInInfo findActiveUserByEmail(String email) {
 		User user = userRepository.findActiveUserByEmail(email)
 			.orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
@@ -29,12 +37,26 @@ public class UserDomainService {
 		return UserSignInInfo.from(user);
 	}
 
+	/**
+	 * 비밀번호 검증
+	 * @param rawPassword 원본 비밀번호
+	 * @param encodedPassword 암호화된 비밀번호
+	 * @throws UserException 일치 하지 않을 시 예외 발생
+	 */
 	public void validPassword(String rawPassword, String encodedPassword) {
 		if (!passwordEncoder.matches(rawPassword, encodedPassword)) {
 			throw new UserException(UserErrorCode.WRONG_PASSWORD);
 		}
 	}
 
+	/**
+	 * 탈퇴되지 않은 유저를 id로 조회<br>
+	 * 조회 시 address도 함께 fetch
+	 * @param userId 유저 id
+	 * @throws UserException 없을 시 예외 발생
+	 * @return 유저의 모든 정보
+	 * @author 이승현
+	 */
 	public UserInfo findActiveUserById(Long userId) {
 		User user = userRepository.findActiveUserById(userId)
 			.orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
@@ -42,6 +64,13 @@ public class UserDomainService {
 		return UserInfo.from(user);
 	}
 
+	/**
+	 * 유저 생성<br>
+	 * domain 가입 유형
+	 * @param userSignUpCommand 유저 가입 정보
+	 * @return 유저의 모든 정보
+	 * @author 이승현
+	 */
 	public UserInfo createUser(UserSignUpCommand userSignUpCommand) {
 		User user = User.builder()
 			.email(userSignUpCommand.email())
@@ -60,12 +89,22 @@ public class UserDomainService {
 		return UserInfo.from(savedUser);
 	}
 
+	/**
+	 * 이메일 검증
+	 * @param email 이메일
+	 * @throws UserException 중복된 이메일이 존재할 시 예외 발생
+	 */
 	public void validEmail(String email) {
 		if (userRepository.existsByEmail(email)) {
 			throw new UserException(UserErrorCode.ALREADY_EXISTS_EMAIL);
 		}
 	}
 
+	/**
+	 * 닉네임 검증
+	 * @param nickname 닉네임
+	 * @throws UserException 중복된 닉네임 존재할 시 예외 발생
+	 */
 	public void validNickname(String nickname) {
 		if (userRepository.existsByNickname(nickname)) {
 			throw new UserException(UserErrorCode.ALREADY_EXISTS_NICKNAME);
