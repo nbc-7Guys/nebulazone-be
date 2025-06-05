414febc5913320c163f89ce0a8bbdcfb40ae78f4
SeungHyunLee054 <yellow13lsh@naver.com>
2025-06-05 20:10:14 +0900

    Test: 테스트 코드 실패 사항 수정 및 테스트 추가
    controller 단위 테스트에 로직이 수정되어 실패되었던 부분 수정, 새로 생긴 controller 메서드에 대한 테스트 추가

--- src/test/java/nbc/chillguys/nebulazone/application/user/controller/UserControllerTest.java
+++ src/test/java/nbc/chillguys/nebulazone/application/user/controller/UserControllerTest.java
@@ -15,7 +15,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -57,9 +59,9 @@ class UserControllerTest {
 		OAuthType.DOMAIN,
 		null,
 		Set.of(new UserResponse.AddressResponse(
-			"address_nickname",
 			"test_road_address",
-			"test_detail_address"
+			"test_detail_address",
+			"address_nickname"
 		)),
 		now,
 		now
@@ -162,8 +164,8 @@ class UserControllerTest {
 
 	@Test
 	@WithCustomMockUser
-	@DisplayName("유저 수정 성공")
-	void success_updateUser() throws Exception {
+	@DisplayName("유저 닉네임, 비밀번호 수정 성공")
+	void success_updateUserNicknameOrPassword() throws Exception {
 		// Given
 		UpdateUserRequest request = new UpdateUserRequest("newNickname",
 			new UpdateUserRequest.PasswordChangeForm("encodedPassword1!", "newPassword1!"));
@@ -208,6 +210,63 @@ class UserControllerTest {
 			);
 
 		verify(userService, times(1)).updateUserNicknameOrPassword(any(), any());
+
+	}
+
+	@Test
+	@WithCustomMockUser
+	@DisplayName("유저 프로필 이미지 수정 성공")
+	void success_updateUserProfileImage() throws Exception {
+		// Given
+		byte[] dummyJpegBytes = new byte[] {(byte)0xFF, (byte)0xD8, (byte)0xFF, (byte)0xE0, 0x00, 0x00};
+
+		MockMultipartFile imagePart = new MockMultipartFile(
+			"profileImage",
+			"profile.jpg",
+			MediaType.IMAGE_JPEG_VALUE,
+			dummyJpegBytes
+		);
+
+		given(userService.updateUserProfileImage(any(), any()))
+			.willReturn(userResponse);
+
+		// When
+		ResultActions perform = mockMvc.perform(multipart("/users")
+			.file(imagePart)
+			.with(r -> {
+				r.setMethod(HttpMethod.PUT.name());
+				return r;
+			}));
+
+		// Then
+		perform.andDo(print())
+			.andExpectAll(
+				status().isOk(),
+				jsonPath("$.userId")
+					.value(1L),
+				jsonPath("$.email")
+					.value("test@test.com"),
+				jsonPath("$.phone")
+					.value("01012345678"),
+				jsonPath("$.nickname")
+					.value("test"),
+				jsonPath("$.profileImageUrl")
+					.value("test_profile_image_url"),
+				jsonPath("$.point")
+					.value(0),
+				jsonPath("$.oAuthType")
+					.value("DOMAIN"),
+				jsonPath("$.addresses[0].addressNickname")
+					.value("address_nickname"),
+				jsonPath("$.addresses[0].roadAddress")
+					.value("test_road_address"),
+				jsonPath("$.addresses[0].detailAddress")
+					.value("test_detail_address"),
+				jsonPath("$.createdAt")
+					.value(now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))),
+				jsonPath("$.modifiedAt")
+					.value(now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+			);
 
 	}
 
