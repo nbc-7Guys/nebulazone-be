12dfb59767d070ce0efaeae3d62f7cc351386880
sukh115 <sukh1151102@gmail.com>
2025-06-10 13:54:06 +0900

    Refactor : 컨트롤러 권한 설정 삭제

--- src/main/java/nbc/chillguys/nebulazone/application/pointhistory/controller/AdminPointHistoryController.java
+++ src/main/java/nbc/chillguys/nebulazone/application/pointhistory/controller/AdminPointHistoryController.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +24,6 @@ import nbc.chillguys.nebulazone.domain.pointhistory.entity.PointHistoryType;
 
 @RestController
 @RequestMapping("/admin/points")
-@PreAuthorize("hasRole('ROLE_ADMIN')")
 @RequiredArgsConstructor
 public class AdminPointHistoryController {
 	private final PointHistoryService pointHistoryService;
