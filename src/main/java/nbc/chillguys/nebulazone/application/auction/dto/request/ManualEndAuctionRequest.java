b0e04820aaf233e7e1d75e8788d5e1cec4f0549e
nagul2 <hayani0000@gmail.com>
2025-06-10 19:39:20 +0900

    Refactor: 입찰 id 벨리데이션 메시지 수정

--- src/main/java/nbc/chillguys/nebulazone/application/auction/dto/request/ManualEndAuctionRequest.java
+++ src/main/java/nbc/chillguys/nebulazone/application/auction/dto/request/ManualEndAuctionRequest.java
@@ -3,7 +3,7 @@ package nbc.chillguys.nebulazone.application.auction.dto.request;
 import jakarta.validation.constraints.NotNull;
 
 public record ManualEndAuctionRequest(
-	@NotNull(message = "입찰자 id는 필수 입력 값 입니다.")
+	@NotNull(message = "입찰id는 필수 입력 값 입니다.")
 	Long bidId
 ) {
 }
