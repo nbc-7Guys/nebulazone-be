e3710a41b72c33683ad0e73d2c35cbc0ce493945
sukh115 <sukh1151102@gmail.com>
2025-06-11 15:57:20 +0900

    Refactor : 빠진 트랜잭션 추가

--- src/main/java/nbc/chillguys/nebulazone/domain/comment/service/AdminCommentDomainService.java
+++ src/main/java/nbc/chillguys/nebulazone/domain/comment/service/AdminCommentDomainService.java
@@ -3,6 +3,7 @@ package nbc.chillguys.nebulazone.domain.comment.service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import nbc.chillguys.nebulazone.application.comment.dto.request.AdminCommentUpdateRequest;
@@ -18,32 +19,36 @@ import nbc.chillguys.nebulazone.domain.comment.repository.CommentRepository;
 public class AdminCommentDomainService {
 	private final CommentRepository commentRepository;
 
+	@Transactional(readOnly = true)
 	public Page<AdminCommentInfo> findComments(AdminCommentSearchQueryCommand command, Pageable pageable) {
 		return commentRepository.searchComments(command, pageable)
 			.map(AdminCommentInfo::from);
 	}
 
+	@Transactional
 	public void updateComment(Long commentId, AdminCommentUpdateRequest request) {
 		Comment comment = findBiCommentId(commentId);
 
 		comment.update(request.content());
 	}
 
-	public Comment findBiCommentId(Long commentId) {
-		return commentRepository.findById(commentId)
-			.orElseThrow(() -> new CommentException(CommentErrorCode.COMMENT_NOT_FOUND));
-	}
-
+	@Transactional
 	public void deleteComment(Long commentId) {
 		Comment comment = findBiCommentId(commentId);
 
 		comment.delete();
 	}
 
+	@Transactional
 	public void restoreComment(Long commentId) {
 		Comment comment = findBiCommentId(commentId);
 
 		comment.restore();
+	}
+
+	public Comment findBiCommentId(Long commentId) {
+		return commentRepository.findById(commentId)
+			.orElseThrow(() -> new CommentException(CommentErrorCode.COMMENT_NOT_FOUND));
 	}
 
 }
