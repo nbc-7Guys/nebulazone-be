e5be94fde270d4cf0660670b4bd28fa4fd41804c
sukh115 <sukh1151102@gmail.com>
2025-06-11 16:16:40 +0900

    Feat : 트랜잭션 전체 조회 및 삭제 구현

---
+++ src/main/java/nbc/chillguys/nebulazone/domain/transaction/service/AdminTransactionDomainService.java
@@ -0,0 +1,34 @@
+package nbc.chillguys.nebulazone.domain.transaction.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import nbc.chillguys.nebulazone.domain.transaction.dto.AdminTransactionInfo;
+import nbc.chillguys.nebulazone.domain.transaction.dto.AdminTransactionSearchQueryCommand;
+import nbc.chillguys.nebulazone.domain.transaction.entity.Transaction;
+import nbc.chillguys.nebulazone.domain.transaction.exception.TransactionErrorCode;
+import nbc.chillguys.nebulazone.domain.transaction.exception.TransactionException;
+import nbc.chillguys.nebulazone.domain.transaction.repository.TransactionRepository;
+
+@Service
+@RequiredArgsConstructor
+public class AdminTransactionDomainService {
+	private final TransactionRepository transactionRepository;
+
+	public Page<AdminTransactionInfo> findTransactions(AdminTransactionSearchQueryCommand command, Pageable pageable) {
+		return transactionRepository.searchTransactions(command, pageable)
+			.map(AdminTransactionInfo::from);
+	}
+
+	public void deleteTransaction(Long txId) {
+		Transaction transaction = findByTransactionId(txId);
+		transactionRepository.delete(transaction);
+	}
+
+	public Transaction findByTransactionId(Long txId) {
+		return transactionRepository.findById(txId)
+			.orElseThrow(() -> new TransactionException(TransactionErrorCode.NOT_FOUNT_TRANSACTION));
+	}
+}
