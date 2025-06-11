5621a45b801d7f54a9096554b29e0eef9d772dda
sukh115 <sukh1151102@gmail.com>
2025-06-11 20:02:09 +0900

    Chore : 주석 추가

--- src/main/java/nbc/chillguys/nebulazone/domain/catalog/service/AdminCatalogDomainService.java
+++ src/main/java/nbc/chillguys/nebulazone/domain/catalog/service/AdminCatalogDomainService.java
@@ -20,11 +20,28 @@ public class AdminCatalogDomainService {
 	private final CustomCatalogAdminRepository adminCatalogQueryRepository;
 	private final CatalogRepository catalogRepository;
 
+	/**
+	 * 어드민 카탈로그 목록을 조건에 맞게 페이징 조회합니다.<br>
+	 * 검색어/타입 등 필터 조건을 적용하여 Catalog 엔티티를 AdminCatalogInfo로 변환합니다.
+	 *
+	 * @param command   도메인 계층 카탈로그 조회 조건 DTO
+	 * @param pageable  페이징 정보
+	 * @return AdminCatalogInfo 목록 Page
+	 * @author 정석현
+	 */
 	public Page<AdminCatalogInfo> findCatalogs(AdminCatalogSearchQueryCommand command, Pageable pageable) {
 		return adminCatalogQueryRepository.searchCatalogs(command, pageable)
 			.map(AdminCatalogInfo::from);
 	}
 
+	/**
+	 * 카탈로그 정보를 수정합니다.
+	 *
+	 * @param catalogId 수정할 카탈로그 ID
+	 * @param request   수정 요청 DTO (이름, 설명, 타입)
+	 * @throws CatalogException 카탈로그가 존재하지 않을 때 발생
+	 * @author 정석현
+	 */
 	public void updateCatalog(Long catalogId, AdminCatalogUpdateRequest request) {
 
 		Catalog catalog = findCatalogId(catalogId);
@@ -32,6 +49,14 @@ public class AdminCatalogDomainService {
 		catalog.update(request.name(), request.description(), request.type());
 	}
 
+	/**
+	 * 카탈로그를 완전 삭제합니다.<br>
+	 * (물리적으로 DB에서 제거)
+	 *
+	 * @param catalogId 삭제할 카탈로그 ID
+	 * @throws CatalogException 카탈로그가 존재하지 않을 때 발생
+	 * @author 정석현
+	 */
 	public void deleteCatalog(Long catalogId) {
 
 		Catalog catalog = findCatalogId(catalogId);
@@ -39,6 +64,15 @@ public class AdminCatalogDomainService {
 		catalogRepository.delete(catalog);
 	}
 
+	/**
+	 * 카탈로그 ID로 엔티티를 조회합니다.<br>
+	 * 존재하지 않을 경우 CatalogException을 발생시킵니다.
+	 *
+	 * @param catalogId 조회할 카탈로그 ID
+	 * @return Catalog 엔티티
+	 * @throws CatalogException 카탈로그가 존재하지 않을 때 발생
+	 * @author 정석현
+	 */
 	public Catalog findCatalogId(Long catalogId) {
 		return catalogRepository.findById(catalogId)
 			.orElseThrow(() -> new CatalogException(CatalogErrorCode.CATALOG_NOT_FOUND));
