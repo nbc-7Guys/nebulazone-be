fa670d9c17bedd3974ad40d0393989abd80c4226
sukh115 <sukh1151102@gmail.com>
2025-06-18 18:11:03 +0900

    refactor: 토스 amount point로 변환

--- src/main/java/nbc/chillguys/nebulazone/application/user/dto/request/TossPaymentConfirmRequest.java
+++ src/main/java/nbc/chillguys/nebulazone/application/user/dto/request/TossPaymentConfirmRequest.java
@@ -1,8 +1,17 @@
 package nbc.chillguys.nebulazone.application.user.dto.request;
 
+import java.util.Map;
+
 public record TossPaymentConfirmRequest(
 	String paymentKey,
 	String orderId,
 	Long point
 ) {
+	public Map<String, Object> toBody() {
+		return Map.of(
+			"paymentKey", paymentKey,
+			"orderId", orderId,
+			"amount", point
+		);
+	}
 }
