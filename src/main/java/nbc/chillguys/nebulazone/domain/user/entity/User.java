6cb7a16cb9c601b66e22fa7576902b66e365ce70
SeungHyunLee054 <yellow13lsh@naver.com>
2025-06-18 17:13:41 +0900

    Refactor: 상속 삭제

--- src/main/java/nbc/chillguys/nebulazone/domain/user/entity/User.java
+++ src/main/java/nbc/chillguys/nebulazone/domain/user/entity/User.java
@@ -8,7 +8,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
@@ -35,7 +34,7 @@ import nbc.chillguys.nebulazone.domain.user.exception.UserException;
 @Table(name = "users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class User extends BaseEntity implements UserDetails {
+public class User extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "user_id")
@@ -167,37 +166,11 @@ public class User extends BaseEntity implements UserDetails {
 		this.point += point;
 	}
 
-	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		return this.roles.stream()
 			.map(role -> new SimpleGrantedAuthority(role.name()))
 			.collect(Collectors.toSet());
 
-	}
-
-	@Override
-	public String getUsername() {
-		return this.email;
-	}
-
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
-
-	@Override
-	public boolean isAccountNonLocked() {
-		return this.status == UserStatus.ACTIVE;
-	}
-
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
-
-	@Override
-	public boolean isEnabled() {
-		return this.status == UserStatus.ACTIVE;
 	}
 
 }
