22aa6fc724a5a4d5d540289495840ca2bedc8768
sukh115 <sukh1151102@gmail.com>
2025-06-20 19:12:22 +0900

    docs: 자바독 주석 추가

--- src/main/java/nbc/chillguys/nebulazone/domain/ban/service/BanDomainService.java
+++ src/main/java/nbc/chillguys/nebulazone/domain/ban/service/BanDomainService.java
@@ -13,10 +13,12 @@ import nbc.chillguys.nebulazone.domain.ban.repository.BanRepository;
 public class BanDomainService {
 	private final BanRepository banRepository;
 
-	public boolean isBanned(String ipAddress) {
-		return banRepository.findActiveBanByIp(ipAddress).isPresent();
-	}
-
+	/**
+	 * 밴 생성 로직
+	 *
+	 * @param command 밴 생성
+	 * @author 정석현
+	 */
 	@Transactional
 	public void createBan(BanCreateCommand command) {
 		if (isBanned(command.ipAddress())) {
@@ -25,4 +27,15 @@ public class BanDomainService {
 		Ban ban = Ban.create(command);
 		banRepository.save(ban);
 	}
+
+	/**
+	 * 밴 유무
+	 * @param ipAddress 아이피 주소
+	 * @return 밴 여부
+	 * @author 정석현
+	 */
+	public boolean isBanned(String ipAddress) {
+		return banRepository.findActiveBanByIp(ipAddress).isPresent();
+	}
+
 }
