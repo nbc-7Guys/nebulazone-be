dc4ce552d671ac132cf0bcef07fd5c720dc3be58
nagul2 <hayani0000@gmail.com>
2025-06-29 16:58:09 +0900

    Refactor: 일부 충돌 해결

--- src/main/java/nbc/chillguys/nebulazone/application/product/service/ProductService.java
+++ src/main/java/nbc/chillguys/nebulazone/application/product/service/ProductService.java
@@ -1,6 +1,5 @@
 package nbc.chillguys.nebulazone.application.product.service;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -35,9 +34,11 @@ import nbc.chillguys.nebulazone.domain.product.dto.ProductUpdateCommand;
 import nbc.chillguys.nebulazone.domain.product.entity.Product;
 import nbc.chillguys.nebulazone.domain.product.entity.ProductEndTime;
 import nbc.chillguys.nebulazone.domain.product.entity.ProductTxMethod;
-import nbc.chillguys.nebulazone.domain.product.event.PurchaseProductEvent;
 import nbc.chillguys.nebulazone.domain.product.service.ProductDomainService;
 import nbc.chillguys.nebulazone.domain.product.vo.ProductDocument;
+import nbc.chillguys.nebulazone.domain.transaction.dto.TransactionCreateCommand;
+import nbc.chillguys.nebulazone.domain.transaction.entity.Transaction;
+import nbc.chillguys.nebulazone.domain.transaction.entity.UserType;
 import nbc.chillguys.nebulazone.domain.transaction.service.TransactionDomainService;
 import nbc.chillguys.nebulazone.domain.user.entity.User;
 import nbc.chillguys.nebulazone.domain.user.service.UserDomainService;
@@ -57,6 +58,7 @@ public class ProductService {
 	private final AuctionRedisService auctionRedisService;
 	private final GcsClient gcsClient;
 	private final ApplicationEventPublisher eventPublisher;
+	private final ProductRepositoryCustomImpl productRepositoryCustomImpl;
 
 	@Transactional
 	public ProductResponse createProduct(User user, Long catalogId, CreateProductRequest request,
@@ -170,9 +172,18 @@ public class ProductService {
 		ProductPurchaseCommand command = ProductPurchaseCommand.of(user, catalog, productId);
 		productDomainService.purchaseProduct(command);
 
-		LocalDateTime purchasedAt = LocalDateTime.now();
-		eventPublisher.publishEvent(new PurchaseProductEvent(user, product, purchasedAt));
-		return PurchaseProductResponse.from(product, purchasedAt);
+		productDomainService.saveProductToEs(product);
+
+		TransactionCreateCommand buyerTxCreateCommand
+			= TransactionCreateCommand.of(user, UserType.BUYER, product, product.getTxMethod().name(),
+			product.getPrice(), product.getCreatedAt());
+		Transaction buyerTx = transactionDomainService.createTransaction(buyerTxCreateCommand);
+		TransactionCreateCommand sellerTxCreateCommand
+			= TransactionCreateCommand.of(product.getSeller(), UserType.SELLER, product, product.getTxMethod().name(),
+			product.getPrice());
+		Transaction sellerTx = transactionDomainService.createTransaction(sellerTxCreateCommand);
+
+		return PurchaseProductResponse.from(buyerTx, sellerTx);
 	}
 
 	public Page<SearchProductResponse> searchProduct(String productName, String sellerNickname,
