f68f2079dfa6e297ad3373b54827965f9aad2b64
sukh115 <sukh1151102@gmail.com>
2025-06-30 19:36:44 +0900

    refactor: 충돌 해결

--- src/main/java/nbc/chillguys/nebulazone/application/auction/service/AuctionAdminService.java
+++ src/main/java/nbc/chillguys/nebulazone/application/auction/service/AuctionAdminService.java
@@ -1,5 +1,8 @@
 package nbc.chillguys.nebulazone.application.auction.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -15,11 +18,16 @@ import nbc.chillguys.nebulazone.domain.auction.dto.AuctionAdminSearchQueryComman
 import nbc.chillguys.nebulazone.domain.auction.dto.AuctionAdminUpdateCommand;
 import nbc.chillguys.nebulazone.domain.auction.entity.Auction;
 import nbc.chillguys.nebulazone.domain.auction.service.AuctionAdminDomainService;
+import nbc.chillguys.nebulazone.domain.product.entity.Product;
+import nbc.chillguys.nebulazone.domain.product.entity.ProductImage;
+import nbc.chillguys.nebulazone.domain.user.entity.User;
+import nbc.chillguys.nebulazone.infra.redis.dto.CreateRedisAuctionDto;
 
 @Service
 @RequiredArgsConstructor
 public class AuctionAdminService {
 	private final AuctionAdminDomainService auctionAdminDomainService;
+	private final AuctionRedisService auctionRedisService;
 
 	public CommonPageResponse<AuctionAdminResponse> findAuctions(AuctionAdminSearchRequest request, Pageable pageable) {
 		AuctionAdminSearchQueryCommand command = new AuctionAdminSearchQueryCommand(
@@ -31,19 +39,36 @@ public class AuctionAdminService {
 		return CommonPageResponse.from(infoPage.map(AuctionAdminResponse::from));
 	}
 
+	@Transactional
 	public void updateAuction(Long auctionId, AuctionAdminUpdateRequest request) {
 		AuctionAdminUpdateCommand command = AuctionAdminUpdateCommand.from(request);
+		auctionRedisService.updateAdminAuction(auctionId, command);
 		auctionAdminDomainService.updateAuction(auctionId, command);
 	}
 
 	@Transactional
 	public void deleteAuction(Long auctionId) {
+		auctionRedisService.deleteAdminAuction(auctionId);
 		auctionAdminDomainService.deleteAuction(auctionId);
 	}
 
+	@Transactional
 	public void restoreAuction(Long auctionId) {
-		auctionAdminDomainService.restoreAuction(auctionId);
-		Auction auction = auctionAdminDomainService.findByAuctionById(auctionId);
+		Auction restoredAuction = auctionAdminDomainService.restoreAuction(auctionId);
+
+		Product product = restoredAuction.getProduct();
+		User user = product.getSeller();
+		LocalDateTime productEndTime = restoredAuction.getEndTime();
+
+		List<String> imageUrls = product.getProductImages()
+			.stream()
+			.map(ProductImage::getUrl)
+			.toList();
+
+		CreateRedisAuctionDto redisDto =
+			new CreateRedisAuctionDto(product, restoredAuction, user, productEndTime, imageUrls);
+
+		auctionRedisService.createAuction(redisDto);
 	}
 
 }
