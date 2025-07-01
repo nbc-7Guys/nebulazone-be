55808950ce302391cd74be9b8f5b894315a547ba
SeungHyunLee054 <yellow13lsh@naver.com>
2025-07-01 20:49:08 +0900

    Refactor: 어노테이션 누락 추가
    @EnableWebSecurity 어노테이션이 누락되어 추가

--- src/main/java/nbc/chillguys/nebulazone/infra/security/config/LocalSecurityConfig.java
+++ src/main/java/nbc/chillguys/nebulazone/infra/security/config/LocalSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,6 +20,7 @@ import nbc.chillguys.nebulazone.infra.security.filter.ExceptionLoggingFilter;
 import nbc.chillguys.nebulazone.infra.security.filter.JwtAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class LocalSecurityConfig {
 	private final CustomAuthenticationEntryPoint entryPoint;
