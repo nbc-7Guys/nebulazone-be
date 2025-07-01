4a4c444ff8f9600ef6c5c7eabb2ae5b8825561e4
SeungHyunLee054 <yellow13lsh@naver.com>
2025-07-01 20:49:15 +0900

    Refactor: csrf 설정 해제
    REST API + JWT(header) 환경에서는 csrf를 사용하지 않는게 맞기때문에 설정을 해제 해줌

--- src/main/java/nbc/chillguys/nebulazone/infra/security/config/SecurityConfig.java
+++ src/main/java/nbc/chillguys/nebulazone/infra/security/config/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -59,16 +58,7 @@ public class SecurityConfig {
 		return http
 			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-			.csrf(csrf -> csrf
-				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-				.ignoringRequestMatchers(
-					"/v3/api-docs/**",
-					"/swagger-ui/**",
-					"/swagger-ui.html",
-					"/webjars/**",
-					"/swagger-resources/**"
-				)
-			)
+			.csrf(AbstractHttpConfigurer::disable)
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers(
 					"/v3/api-docs/**",
