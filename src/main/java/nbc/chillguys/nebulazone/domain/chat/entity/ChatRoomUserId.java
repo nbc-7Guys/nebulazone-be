eb1015ecd97fa15fe934ea669ea25aa5fbc2265d
uguls <chobozaex@gmail.com>
2025-07-01 23:02:30 +0900

    Refactor: EqualsAndHashCode 추가

--- src/main/java/nbc/chillguys/nebulazone/domain/chat/entity/ChatRoomUserId.java
+++ src/main/java/nbc/chillguys/nebulazone/domain/chat/entity/ChatRoomUserId.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 @Builder
 public class ChatRoomUserId implements Serializable {
 
